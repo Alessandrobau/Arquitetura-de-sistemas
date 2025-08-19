9db83079d2a84e4d936ebbb12370e3ebcbb13ef9
Alessandrobau <alessandrobau011@gmail.com>
2025-08-19 15:43:59 +0000

    insomnia requests

--- models/productModel.js
+++ models/productModel.js
@@ -1,8 +1,8 @@
 let products = [
     { id: 1, nome: 'Notebook', preco: 3500.00, estoque: 5 },
-    { id: 2, nome: 'Mouse sem fio', preco: 89.90, estoque: 10 },
-    { id: 3, nome: 'Teclado Mecânico', preco: 250.00, estoque: 3 },
-    { id: 4, nome: 'Monitor 24"', preco: 950.00, estoque: 0 }
+    { id: 2, nome: 'Mouse', preco: 89.90, estoque: 10 },
+    { id: 3, nome: 'Teclado', preco: 250.00, estoque: 3 },
+    { id: 4, nome: 'Monitor"', preco: 950.00, estoque: 1 }
 ];
 
 const Product = {
