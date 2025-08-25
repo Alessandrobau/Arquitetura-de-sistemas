b9d72a46d5d55f630aee182da99d723df70f1313
GabrielRoggia <gabee.roggia@gmail.com>
2025-08-25 20:40:02 -0300

    find by id

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,11 +2,32 @@ const Product = require('../models/productModel');
 const Order = require('../models/orderModel');
 
 const orderController = {
+    //listar todos os pedidos
     listOrders: (req, res) => {
         const orders = Order.findAll();
         res.json(orders);
     },
 
+    // listar por ID
+    listOrdersById: (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const order = Order.findById(id);
+
+            if (!order) {
+                return res.status(404).json({ message: 'Pedido nao encontrado. '});
+            }
+
+            res.status(200).json(order);
+
+        } catch (error) {
+            console.error('Erro ao buscar pedido por ID: ', error);
+            res.status(500).json({ message: 'Ocorreu um erro no servidor '});
+        }
+    },
+
+    //criar novo pedido
     createOrder: (req, res) => {
         const { cliente_id, itens } = req.body;
 
@@ -22,11 +43,13 @@ const orderController = {
         for (const item of itens) {
             const product = Product.findById(item.product_id);
 
+            // verificar se existe
             if (!product) {
                 stockErros.push(`product com ID ${item.product_id} não encontrado.`);
                 continue;
             }
-
+            
+            // verificar se tem no estoque
             if (product.estoque < item.quantidade) {
                 stockErros.push(`Estoque insuficiente para o product '${product.nome}'. Solicitado: ${item.quantidade}, Disponível: ${product.estoque}.`);
             } else {
